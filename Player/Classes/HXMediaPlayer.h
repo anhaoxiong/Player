5ab0cb0bb344776cfef1dbe0546ac94578ad61b9
anhaoxiong <hxiongan@gmail.com>
2018-01-08 16:18:47 +0800

    first commit

---
+++ Player/Classes/HXMediaPlayer.h
@@ -0,0 +1,12 @@
+//
+//  HXMediaPlayer.h
+//  Player
+//
+//  Created by hxiongan on 2018/1/8.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXMediaPlayer : NSObject
+
+@end
---
+++ Player/Classes/HXMediaPlayer.m
@@ -0,0 +1,12 @@
+//
+//  HXMediaPlayer.m
+//  Player
+//
+//  Created by hxiongan on 2018/1/8.
+//
+
+#import "HXMediaPlayer.h"
+
+@implementation HXMediaPlayer
+
+@end
